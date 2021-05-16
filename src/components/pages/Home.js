69f15c03379a3cccddc1fc06efadb8e4fd3f3e40
Amol Jagtap <amoljagtap17@gmail.com>
2021-05-16 21:13:36 +0530

    MUI_BLOG: code refactor

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,3 +1,9 @@
+import { Typography } from '@material-ui/core'
 import { Title } from 'components/ui'
 
-export const Home = () => <Title titleText="Home Page" />
+export const Home = () => (
+  <>
+    <Title titleText="Home Page" />
+    <Typography variant="h6">Checkout the Blogs route :P</Typography>
+  </>
+)
